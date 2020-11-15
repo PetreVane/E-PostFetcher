2cf64bf843768be1ab07dcd7dd121b64010fd9af
Petre Vane <petre.vane@gmail.com>
2020-11-15 17:02:11 +0100

    renamed

--- src/com/orbSec/view/Coordinator.java
+++ src/com/orbSec/view/Coordinator.java
@@ -3,6 +3,7 @@ package com.orbSec.view;
 import com.orbSec.EmailManager;
 import com.orbSec.controller.BaseController;
 import com.orbSec.controller.LoginWindowController;
+import com.orbSec.controller.MainWindowViewController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -13,7 +14,8 @@ import java.io.IOException;
 public class ViewFactory {
 
     EmailManager emailManager;
-    private String fxmlPath = "LoginWindow.fxml";
+    private String loginFxmlPath = "LoginWindow.fxml";
+    private String mainFxmlPath = "MainWindow.fxml";
 
     public ViewFactory(EmailManager emailManager) {
         this.emailManager = emailManager;
@@ -21,9 +23,22 @@ public class ViewFactory {
 
     public void presentLoginScreen() {
         System.out.println("Login screen presented");
-        BaseController loginController = new LoginWindowController(emailManager, this, fxmlPath);
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(loginController.getFxmlName()));
-        fxmlLoader.setController(loginController);
+        BaseController loginController = new LoginWindowController(emailManager, this, loginFxmlPath);
+        setStageFor(loginController);
+
+    }
+
+    public void presentMainScreen() {
+        System.out.println("presetMainScreen method called");
+        BaseController mainController = new MainWindowViewController(emailManager, this, mainFxmlPath);
+        setStageFor(mainController);
+
+    }
+
+    private void setStageFor(BaseController controller) {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
+        fxmlLoader.setController(controller);
 
         Parent parent;
 
@@ -39,6 +54,5 @@ public class ViewFactory {
         Stage stage = new Stage();
         stage.setScene(scene);
         stage.show();
-        
     }
 }
