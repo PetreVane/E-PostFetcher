fdd0ada0a76a2b6f3b270bbb281bb6c8f9effbdf
Petre Vane <petre.vane@gmail.com>
2020-12-01 18:06:03 +0100

    Subclassed TreeItem & declared new object responsible with fetching remote directories from server

---
+++ src/com/orbSec/models/CustomTreeItem.java
@@ -0,0 +1,5 @@
+package com.orbSec.models;/**
+ * @project EmailClient
+ * @author Created by Petre Vane on 01/12/2020
+ */public class CustomTreeItem {
+}
---
+++ src/com/orbSec/models/services/RemoteDirectories.java
@@ -0,0 +1,5 @@
+package com.orbSec.models.services;/**
+ * @project EmailClient
+ * @author Created by Petre Vane on 01/12/2020
+ */public class RemoteDirectories {
+}
